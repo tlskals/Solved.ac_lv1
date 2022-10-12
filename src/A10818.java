2a516b5fb874757ff482c55cb03967327546947e
ChoiJiMin <chlwlals8708@gmail.com>
2022-10-13 00:28:36 +0900

    22.10.13

---
+++ src/A10818.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class A10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int i = 0;
+        int[] arr = new int[a];
+        while(st.hasMoreTokens()) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            i++;
+        }
+
+        Arrays.sort(arr);
+        bw.write(arr[0] + " " + arr[a - 1]);
+        bw.flush();
+        bw.close();
+    }
+}
--- src/A2562.java
+++ src/A2562.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class A2562 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //string
 
         int[] num = new int[9]; // 입력받을 숫자가 9개로 고정이므로 크기가 9인 배열 생성
         for (int i = 0; i < 9; i++) {
@@ -13,7 +14,7 @@ public class A2562 {
         int max = 0; //최대값을 넣어줄 변수 선언
         int count = 0; // 몇번째에 입력되어 있는지 세어줄 변수 선언
 
-        for (int j = 0; j < 9; j++) { // 배열을 순회할 반복문 선언
+        for (int j = 1; j < 9; j++) { // 배열을 순회할 반복문 선언 0~8
             if (num[j] > max) { // 배열의 0번째 요소부터 8번째 요소까지 돌아가면서 max값과 비교하면서 배열의 요소가 max값보다 크면
                 max = num[j]; // 해당 배열의 요소를 max값으로 교체
                 count = j+1; // count 값은 해당 배열의 번호+1로 교체 (배열은 0번부터 시작이니 +1을 해주는것)
