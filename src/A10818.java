7fa4db794be2be849b8280b219735476f0dae4a8
ChoiJiMin <chlwlals8708@gmail.com>
2022-10-17 09:07:11 +0900

    22.10.17

--- src/A10818.java
+++ src/A10818.java
@@ -16,7 +16,7 @@ public class A10818 {
         // StringTokenizer는 br.readLine() 구분자를 적지 않으면 자동으로 공백 기준으로 나눔
 
         int i = 0;  // 변수 i 선언
-        int[] arr = new int[a]; // 빈배열 arr 선언, 크기는 첫번째 입력된 숫자 만큼
+        int[] arr = new int[a]; // 빈배열 arr 선언, 크기는 첫번째 입력된 숫자 만큼//
         while(st.hasMoreTokens()) {  // 반복문 실행, hasMoreTokens는 boolean 타입 (true or false)
             arr[i] = Integer.parseInt(st.nextToken()); // 15번째 줄에서 선언한 st에 token이 있다면 배열 arr에 넣어줌
             i++;
