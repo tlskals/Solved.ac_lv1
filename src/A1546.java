41fd52dee089ea23a4d9ae9cbaa4540ee3a43f88
ChoiJiMin <chlwlals8708@gmail.com>
2022-10-23 02:23:52 +0900

    22.10.23

--- src/A1546.java
+++ src/A1546.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
-public class A1157 {
+public class A1546 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
