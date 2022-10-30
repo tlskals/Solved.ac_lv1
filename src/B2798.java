ed41336d1dae5427c0eeed3ca3d5b87946f8a76c
ChoiJiMin <chlwlals8708@gmail.com>
2022-10-30 11:12:42 +0900

    22.10.30

--- src/B2798.java
+++ src/B2798.java
@@ -18,13 +18,11 @@ public class B2798 {
         int[] arr = new int[count]; // 각 카드들의 값을 모두 순회하기위한 배열 arr 선언
 
         StringTokenizer st2 = new StringTokenizer(br.readLine()); //둘째 줄 입력값을 StringTokenizer로 받아옴
-
         for (int i = 0; i<count; i++) { // 반복문을 통해 arr에 카드에 적혀있는 값들을 입력
             arr[i] = Integer.parseInt(st2.nextToken());
         }
 
         br.close();
-
         int result = 0; // 최종값을 담아줄 변수 result 선언
 
         for (int j = 0; j < count - 2; j++) { // 세장의 카드를 골라야 하기때문에 count-2까지만 반복문 사용
